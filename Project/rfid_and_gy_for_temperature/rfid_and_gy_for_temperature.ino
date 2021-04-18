de2807ce3ea8916aee2c61c60aed6a697e33d14b
muditjains <muditjains@yahoo.com>
2021-04-18 18:34:08 -0400

    Made some changes to the wrong RFID input

--- Project/rfid_and_gy_for_temperature/rfid_and_gy_for_temperature.ino
+++ Project/rfid_and_gy_for_temperature/rfid_and_gy_for_temperature.ino
@@ -1,48 +1,14 @@
-/*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- */
+
 
 #include <SPI.h>
 #include <MFRC522.h>
 #include "Wire.h"
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
+#define RST_PIN         9          
+#define SS_PIN          10         
 const int MPU_ADDR = 0x68;
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-String tagUID = "2C 34 41 33";  // String to store UID of tag. Change it with your tag's UID
-
+String tagUID = "2C 34 41 33";  // This is for the keyfob.
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
 int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
 int16_t temperature; // variables for temperature data
@@ -54,6 +20,7 @@ void setup() {
   mfrc522.PCD_Init();   // Init MFRC522
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  // pins setup
   pinMode(8,OUTPUT) ; 
   pinMode(7,OUTPUT) ; 
   pinMode(6,OUTPUT) ; 
@@ -91,12 +58,6 @@ void loop() {
   if (tag.substring(1) == tagUID) //change here the UID of the card/cards that you want to give access
   {
       Serial.println(F("Granted"));
-//      digitalWrite(6,HIGH);
-//      digitalWrite(5,HIGH);
-//      digitalWrite(4,HIGH);
-//      digitalWrite(3,HIGH);
-//      digitalWrite(2,HIGH);
-
       digitalWrite(8,HIGH);
       delay(2000);
       digitalWrite(8,LOW);
@@ -106,9 +67,6 @@ void loop() {
        Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
        Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
 
-
-
-       
 
         accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
         accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
@@ -158,9 +116,26 @@ void loop() {
   else
   {
           Serial.println(F("Denied"));
+
+
+          // warning the user for the wrong card.
           digitalWrite(7,HIGH);
-          delay(2000);
+          delay(500);
           digitalWrite(7,LOW);
+          delay(500);
+          digitalWrite(7,HIGH);
+          delay(500);
+          digitalWrite(7,LOW);
+          delay(500);
+          digitalWrite(7,HIGH);
+          delay(500);
+          digitalWrite(7,LOW);
+          delay(500);
+          digitalWrite(7,HIGH);
+          delay(500);
+          digitalWrite(7,LOW);
+
+          
           digitalWrite(6,LOW);
           digitalWrite(5,LOW);
           digitalWrite(4,LOW);
@@ -168,6 +143,5 @@ void loop() {
           digitalWrite(2,LOW);
           
   }
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-//  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  
 }
